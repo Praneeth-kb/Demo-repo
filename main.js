633ec0c714295d1e131ecaa8d41ae002fb75137d
Praneeth-kb <praneethkb587@gmail.com>
2024-05-15 22:20:25 +0530

    Adding project files
    along files readme

---
+++ main.js
@@ -0,0 +1,10 @@
+// let firstName = "Praneeth ";
+// let LastName  = "K B";
+// console.log("Hi "+ firstName + LastName);
+
+let sum=0;
+for(i=0;i<10000000000;i++){
+sum=sum+i;
+}
+console.log(sum);
+
